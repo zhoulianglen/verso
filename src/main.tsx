81e23cae04f0348eb815342c867468673c1163db
周良 <wwwzhouliang@gmail.com>
2026-02-23 13:22:58 +0800

    feat: add Aptabase analytics (app_opened tracking)

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./App.css";
+import { init, trackEvent } from "@aptabase/web";
+
+init("A-US-7078790453");
+trackEvent("app_opened");
 
 class ErrorBoundary extends React.Component<
   { children: React.ReactNode },
